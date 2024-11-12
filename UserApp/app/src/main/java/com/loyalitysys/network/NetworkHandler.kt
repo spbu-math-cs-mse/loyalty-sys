0091b3250e7d68dd96188f40cbcf4c9fc6c3f372
Artyom Peshkov <artemyipeshkov@gmail.com>
2024-11-12 20:23:50 +0300

    done

--- UserApp/app/src/main/java/com/loyalitysys/network/NetworkHandler.kt
+++ UserApp/app/src/main/java/com/loyalitysys/network/NetworkHandler.kt
@@ -4,8 +4,6 @@ package com.example.loyalitysys.network
 
 import android.util.Log
 import android.widget.TextView
-import com.loyalitysys.network.PurchaseResponse
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -16,28 +14,20 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 object NetworkSetting {
-    const val baseUrl = "http://84.201.154.253:1235/"
+    const val baseUrl = "http://adress:port/"
 }
 
-data class Product(val name: String, val price: String)
+data class Product(val product_id: String, val quantity: String)
 
 data class PurchaseRequest(val user_id: String, val products: List<Product>)
 
 interface ApiService {
-    @POST("purchase")
-    fun getLastPurchase(@Body body: PurchaseRequest): Call<PurchaseResponse>
+    @POST("/user/user_id/purchase")
+    fun getLastPurchase(@Body body: PurchaseRequest): Call<Unit>
 }
 
-class NetworkHandler(val levelTextView: TextView, val saleTextView: TextView, shouldFake: Boolean = false) {
-    init {
-        if (!shouldFake) {
-            fetchLastPurchase();
-        } else {
-            fakeLastPurchase();
-        }
-    }
-
-    private fun fetchLastPurchase() {
+object NetworkHandler {
+    fun sendLastPurchase(user_id: String, products: List<Product> = listOf(Product("T-shirt", (1).toString()))) {
         val retrofit = Retrofit.Builder()
             .baseUrl(NetworkSetting.baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
@@ -45,48 +35,18 @@ class NetworkHandler(val levelTextView: TextView, val saleTextView: TextView, sh
 
         val apiService = retrofit.create(ApiService::class.java)
         GlobalScope.launch {
-            delay(1000L * (3..9).random())
-            for (purchaseNumber in 1..3) {
-                apiService.getLastPurchase(PurchaseRequest("User0", listOf(Product("Random", (1000L).toString())))).enqueue(object : Callback<PurchaseResponse> {
+                apiService.getLastPurchase(PurchaseRequest(user_id, products)).enqueue(object : Callback<Unit> {
                     override fun onResponse(
-                        call: Call<PurchaseResponse>,
-                        response: Response<PurchaseResponse>
+                        call: Call<Unit>,
+                        response: Response<Unit>
                     ) {
                         Log.d("Got response", response.code().toString())
-                        var responseBody = response.body()!!
-                        responseBody.total_cost = responseBody.total_cost * purchaseNumber;
-                        Log.d("Got response", responseBody.toString())
-                        if (!response.isSuccessful) {
-                            return;
-                        }
-                        response.body()?.let { purchase ->
-                            val level: Int = (purchase.total_cost / 1000).toInt()
-                            updateViews(level + 1, level * 5)
-                        }
                     }
 
-                    override fun onFailure(call: Call<PurchaseResponse>, t: Throwable) {
+                    override fun onFailure(call: Call<Unit>, t: Throwable) {
                         Log.e("Request failed", t.toString())
                     }
                 })
-                delay(10000)
-            }
         }
     }
-
-    private fun updateViews(level: Int, sale: Int) =
-        GlobalScope.launch(Dispatchers.Main.immediate) {
-            levelTextView.text = "Loyality Level: ${level}"
-            saleTextView.text = "Sale: ${sale}%"
-        }
-
-    private fun fakeLastPurchase() =
-        GlobalScope.launch {
-            updateViews(1, 1)
-            delay(10000)
-            updateViews(3, 5)
-            delay(10000)
-            updateViews(5, 15)
-    }
-
 }
--- UserApp/app/src/main/java/com/loyalitysys/network/PurchaseResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.loyalitysys.network
-
-data class PurchaseResponse(
-    val user_id: String,
-    var total_cost: Long
-)
