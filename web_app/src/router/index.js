df67ef192146207b419c7739f00a04cbb8fd035e
Maruf <sedovlas777a@gmail.com>
2024-12-17 22:51:08 +0500

    refactored code in router

--- web_app/src/router/index.js
+++ web_app/src/router/index.js
@@ -1,11 +1,9 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
-    // component: HomeView,
     redirect: { name: "stats" },
     meta: {
       title: "Главная",
@@ -20,14 +18,6 @@ const routes = [
     },
   },
   {
-    path: "/coupons",
-    name: "coupons",
-    component: () => import("../views/CouponsView.vue"),
-    meta: {
-      title: "Купоны",
-    },
-  },
-  {
     path: "/privilege",
     name: "privilege",
     component: () => import("../views/PrivilegeView.vue"),
@@ -36,11 +26,27 @@ const routes = [
     },
   },
   {
-    path: "/settings",
-    name: "settings",
-    component: () => import("../views/SettingsView.vue"),
+    path: "/events",
+    name: "events",
+    component: () => import("../views/EventsView.vue"),
     meta: {
-      title: "Настройки",
+      title: "События",
+    },
+  },
+  {
+    path: "/admins",
+    name: "admins",
+    component: () => import("../views/AdminsView.vue"),
+    meta: {
+      title: "Админы",
+    },
+  },
+  {
+    path: "/coupons",
+    name: "coupons",
+    component: () => import("../views/CouponsView.vue"),
+    meta: {
+      title: "Купоны",
     },
   },
   {
@@ -60,15 +66,6 @@ const routes = [
     },
   },
   {
-    path: "/sales",
-    name: "sales",
-    redirect: { name: "notfound" },
-    // component: () => import("../views/SalesView.vue"),
-    meta: {
-      title: "Скидки",
-    },
-  },
-  {
     path: "/:catchAll(.*)",
     redirect: { name: "notfound" },
   },
@@ -81,20 +78,15 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   const auth = JSON.parse(localStorage.getItem("auth")) || false;
-  console.log(`Перехожу на маршрут: ${to.fullPath}`);
-  
-  if (to.name !== 'login' && !auth) {
-    next({ name: "login" })
-  }
-  else if (to.name === 'login' && auth) {
-    next({ name: "home" })
-  }
-  else {
+
+  if (to.name !== "login" && !auth) {
+    next({ name: "login" });
+  } else if (to.name === "login" && auth) {
+    next({ name: "home" });
+  } else {
     next();
     document.title = to.meta.title || "Система лояльности";
   }
-  
-
 });
 
 export default router;
